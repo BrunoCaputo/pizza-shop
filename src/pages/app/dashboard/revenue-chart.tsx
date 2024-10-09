5f770ae649b1995404a2f8bf1ea4c5033641bce0
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-09 13:41:20 -0300

    feat: Dashboard charts
    Using recharts lib

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,97 @@
+import { CSSProperties } from 'react'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    date: '08/10',
+    revenue: 1200,
+  },
+  {
+    date: '09/10',
+    revenue: 1000,
+  },
+  {
+    date: '10/10',
+    revenue: 800,
+  },
+  {
+    date: '11/10',
+    revenue: 400,
+  },
+  {
+    date: '12/10',
+    revenue: 2400,
+  },
+  {
+    date: '13/10',
+    revenue: 1050,
+  },
+  {
+    date: '14/10',
+    revenue: 500,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 } as CSSProperties}>
+            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
+            <Tooltip />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet['500']}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
