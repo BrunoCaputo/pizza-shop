771ec6007d40839d53aca872c493be4ad7a49f04
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-10 11:06:11 -0300

    feat: Passwordless sing in

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -24,16 +26,18 @@ export function SignIn() {
     resolver: zodResolver(signInForm),
   })
 
-  async function handleSignIn(data: SignInForm) {
-    try {
-      console.log(data)
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
 
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+  async function handleSignIn({ email }: SignInForm) {
+    try {
+      await authenticate({ email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data),
+          onClick: () => handleSignIn({ email }),
         },
       })
     } catch (error) {
