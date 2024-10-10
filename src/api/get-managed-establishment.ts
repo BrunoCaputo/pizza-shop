e9d9464e93733c9288f31fa8624b7aa8f93e08f9
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-10 13:38:27 -0300

    feat: Get establishment and manager data

---
+++ src/api/get-managed-establishment.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/axios'
+
+interface GetManagedEstablishmentResponse {
+  id: string
+  name: string
+  createdAt: Date | null
+  updatedAt: Date | null
+  description: string | null
+  managerId: string | null
+}
+
+export async function getManagedEstablishment(): Promise<GetManagedEstablishmentResponse> {
+  const response = await api.get<GetManagedEstablishmentResponse>(
+    '/managed-restaurant',
+  )
+
+  return response.data
+}
